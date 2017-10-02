41a4f606f7151f8f07f2f52b1f63c96d0aa935de
Carl Pearson <pearson@illinois.edu>
2017-10-02 12:24:54 -0500

    .

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ SRC_DIR="./ece408_src"
 SRCS=`find $SRC_DIR -type f`
 
 # Where MXNet source lives 
-MXNET_SRC_ROOT="./2017fa_ece408_mxnet_skeleton"
+MXNET_SRC_ROOT="$HOME/repos/2017fa_ece408_mxnet_skeleton"
 
 # Copy our files to the custom operator directory
 for src in $SRCS; do
@@ -16,7 +16,7 @@ for src in $SRCS; do
 done
 
 # Build MXNet
-nice -n20 make -j`nproc` -C 2017fa_ece408_mxnet_skeleton
+nice -n20 make -j`nproc` -C "$MXNET_SRC_ROOT"
 
 # Install python bindings
-pip install --user -e $MXNET_SRC_ROOT/python
+pip install --user -e "$MXNET_SRC_ROOT/python"
