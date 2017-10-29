ab6a1858b235e965166f293c98efe1b2f289a171
Carl Pearson <carl.w.pearson@gmail.com>
2017-10-29 15:17:19 -0500

    Add a draft python script that should be used during final submission

---
+++ final.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import mxnet as mx
+import logging
+from reader import load_mnist
+
+# Log to stdout for MXNet
+logging.getLogger().setLevel(logging.DEBUG)  # logging to stdout
+
+print "Loading fashion-mnist data...",
+test_images, test_labels = load_mnist(path="/fashion-mnist", kind="t10k")
+test_images = test_images.reshape((10000, 1, 28, 28))
+test_labels = test_labels.reshape(10000)
+print "done"
+
+# Do everything in a single batch
+batch_size = len(test_images)
+
+# Get iterators that cover the dataset
+test_iter = mx.io.NDArrayIter(
+    test_images, test_labels, batch_size)
+
+# Evaluate the network
+print "Loading model...",
+lenet_model = mx.mod.Module.load(
+    prefix='/models/ece408-high', epoch=1, context=mx.gpu())
+lenet_model.bind(data_shapes=test_iter.provide_data,
+                 label_shapes=test_iter.provide_label)
+print "done"
+
+acc = mx.metric.Accuracy()
+lenet_model.score(test_iter, acc)
+print(acc)
