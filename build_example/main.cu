6eea4c8670f251da08c8514249b50cfb717b2c7d
Carl Pearson <pearson@illinois.edu>
2017-10-30 16:18:14 -0500

    Add some CUDA call to the build_example

--- build_example/main.cu
+++ build_example/main.cu
@@ -2,4 +2,16 @@
 
 int main(void) {
     fprintf(stdout, "Hello world\n");
+
+    int deviceCount = 0;
+    cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+
+    if (error_id != cudaSuccess) {
+        printf("cudaGetDeviceCount returned %d\n-> %s\n", (int)error_id, cudaGetErrorString(error_id));
+        printf("Result = FAIL\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Found %d GPUs\n");
+    }
+
 }
