bad7d6a95a6fff93bf891b93bf9842e1b0982f3a
Carl Pearson <pearson@illinois.edu>
2017-11-28 15:13:36 -0600

    Add example use of macros to comment

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -25,6 +25,10 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     const int W_out = W - K + 1;
     (void)H_out; // silence declared but never referenced warning. remove this line when you start working
     (void)W_out; // silence declared but never referenced warning. remove this line when you start working
+
+    // An example use of these macros:
+    // float a = y4d(0,0,0,0)
+    // y4d(0,0,0,0) = a
     #define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
     #define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
     #define k4d(i3,i2,i1,i0) k[(i3) * (C * K * K) + (i2)*(K * K) + (i1)*(K) + i0]
