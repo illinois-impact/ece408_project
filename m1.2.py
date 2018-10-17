c66c3300aac9be703cef3f809fc0b071386eee6e
msharmavikram <vsm2@illinois.edu>
2018-10-16 23:27:23 -0500

    updates for 72x72 input image

--- m1.2.py
+++ m1.2.py
@@ -7,9 +7,9 @@ from reader import load_mnist
 # Log to stdout for MXNet
 logging.getLogger().setLevel(logging.DEBUG)  # logging to stdout
 
-print "Loading fashion-mnist data..."
+print "Loading fashion-mnist data...",
 test_images, test_labels = load_mnist(
-    path="/fashion-mnist", rows=64, cols=64, kind="t10k-64")
+    path="/fashion-mnist", rows=72, cols=72, kind="t10k-72")
 print "done"
 
 # Do everything in a single batch
@@ -20,7 +20,7 @@ test_iter = mx.io.NDArrayIter(
     test_images, test_labels, batch_size)
 
 # Evaluate the network
-print "Loading model..."
+print "Loading model...",
 lenet_model = mx.mod.Module.load(
     prefix='/models/baseline', epoch=2, context=mx.gpu())
 lenet_model.bind(data_shapes=test_iter.provide_data,
