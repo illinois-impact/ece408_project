e2de7724e597f3c25275bbf53997706432521a25
skota121 <sankruth.kota@gmail.com>
2018-11-24 05:06:58 -0500

    milestones 1-3

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -1,7 +1,7 @@
 
 #ifndef MXNET_OPERATOR_NEW_FORWARD_CUH_
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
-
+#include <cmath>
 #include <mxnet/base.h>
 
 namespace mxnet
@@ -9,7 +9,7 @@ namespace mxnet
 namespace op
 {
 
-__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
+__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K, const int W_grid)
 {
 
     /*
@@ -27,11 +27,30 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 // An example use of these macros:
 // float a = y4d(0,0,0,0)
 // y4d(0,0,0,0) = a
+
+#define TILE_WIDTH 16
 #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
 #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-    
+int n, m, h, w, c, p, q;
+
+n = blockIdx.x;
+m = blockIdx.y;
+h = (blockIdx.z/W_grid)*TILE_WIDTH + threadIdx.y;
+w = (blockIdx.z % W_grid)*TILE_WIDTH + threadIdx.x;
+
+float acc = 0;
+if(h < H_out && w < W_out) {
+    for(c = 0; c < C; c++ ) {
+        for(p=0; p < K; p++) {
+            for(q = 0; q < K; q++) {
+                acc += x4d(n, c, h+p, w+q) * k4d(m, c, p, q);
+            }
+        }
+    }
+    y4d(n, m, h, w) = acc;
+}
 
 #undef y4d
 #undef x4d
@@ -49,17 +68,29 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     // Use mxnet's CHECK_EQ to do assertions.
     // Remove this assertion when you do your implementation!
-    CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
+    // CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     // Extract the tensor dimensions into B,M,C,H,W,K
     // ...
+    const int B = x.shape_[0];
+    const int M = y.shape_[1];
+    const int C = x.shape_[1];
+    const int H = x.shape_[2];
+    const int W = x.shape_[3];
+    const int K = w.shape_[3];
+
+    int H_out = H - K + 1;
+    int W_out = W - K + 1;
+    int W_grid = ceil((float)(((W_out-1) / TILE_WIDTH)+1));
+    int H_grid = ceil((float)(((H_out-1)/ TILE_WIDTH)+1));
+    int Z = W_grid * H_grid;
 
     // Set the kernel dimensions
-    // dim3 gridDim(0);
-    // dim3 blockDim(0);
+    dim3 gridDim(B, M, Z);
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
 
     // Call the kernel
-    // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K, W_grid);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
--- ece408_src/new-forward.h
+++ ece408_src/new-forward.h
@@ -21,22 +21,38 @@ void forward(mshadow::Tensor<cpu, 4, DType> &y, const mshadow::Tensor<cpu, 4, DT
     */
 
     const int B = x.shape_[0];
-    // const int M = y.shape_[1];
-    // const int C = x.shape_[1];
-    // const int H = x.shape_[2];
-    // const int W = x.shape_[3];
-    // const int K = w.shape_[3];
+    const int M = y.shape_[1];
+    const int C = x.shape_[1];
+    const int H = x.shape_[2];
+    const int W = x.shape_[3];
+    const int K = k.shape_[3];
 
-    for (int b = 0; b < B; ++b) {
+      int H_out = H - K + 1;
+      int W_out = W - K + 1;
+        for (int b = 0; b < B; ++b)   {               // for each image in batch
+          for(int m = 0;  m < M;  m++) {             // for each output feature map
+            for(int h = 0; h < H_out; h++) {          // for each output element
+              for(int w = 0; w < W_out; w++) {
+                y[m][h][w] = 0;
+                for(int c = 0;  c < C; c++)    {     // sum over all input feature maps
+                  for(int p = 0; p < K; p++)	  {     // KxK  filter
+                    for(int q = 0; q < K; q++) {
+                      y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
+             }
+           }
+         }
+       }
+     }
+   }
+ }
 
-        CHECK_EQ(0,1) << "Remove this line and replace it with your implementation.";
-
-        /* ... a bunch of nested loops later...
-            y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
-        */
+        // CHECK_EQ(0,1) << "Remove this line and replace it with your implementation.";
+        //
+        // /* ... a bunch of nested loops later...
+        //     y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
+        // */
     }
 
-}
 }
 }
 
